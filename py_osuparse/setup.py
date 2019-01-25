a34b2ecc7e77f51822c876bcc570752364c26db0
Rafael Castillo <rafael@trfs.me>
2019-01-25 06:36:49 -0300

    Minor version

--- py_osuparse/setup.py
+++ py_osuparse/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="osuparse",
-    version="2.0.0",
+    version="2.0.1",
     rust_extensions=[RustExtension("osuparse.osuparse",
                      binding=Binding.RustCPython)],
     packages=["osuparse"],
