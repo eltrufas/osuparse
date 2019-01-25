4f972275b06fcda6e78fbff8c0d5a67392def34a
Rafael Castillo <rafael@trfs.me>
2019-01-25 05:07:44 -0300

    Version 2.0.0

--- py_osuparse/setup.py
+++ py_osuparse/setup.py
@@ -3,8 +3,9 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="osuparse",
-    version="0.1.0",
-    rust_extensions=[RustExtension("osuparse.osuparse", binding=Binding.RustCPython)],
+    version="2.0.0",
+    rust_extensions=[RustExtension("osuparse.osuparse",
+                     binding=Binding.RustCPython)],
     packages=["osuparse"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
